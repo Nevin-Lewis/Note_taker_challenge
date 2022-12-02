538b130fcd63f10e41ae933ae38a554bda672837
Nevin Lewis <lewisnevin@utexas.edu>
2022-12-01 22:09:36 -0600

    attempt to correct pathway error

--- scripts.js
+++ scripts.js
@@ -3,13 +3,13 @@ const fs = require('fs');
 const path = require('path');
 const util = require('util');
 const uuid = require('./helpers/uuid');
-const api = require('./public/assets/js/index.js');
+// const api = require('./public/assets/js/index.js');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
+// app.use('/api', api);
 
 const readFromFile = util.promisify(fs.readFile);
 
