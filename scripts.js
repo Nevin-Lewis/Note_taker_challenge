3320182f06e52ab5e93a71e1f24407643d0df994
Nevin Lewis <lewisnevin@utexas.edu>
2022-12-01 21:52:27 -0600

    added api

--- scripts.js
+++ scripts.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const path = require('path');
 const util = require('util');
 const uuid = require('./helpers/uuid');
+const api = require('./public/assets/js/index.js');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
